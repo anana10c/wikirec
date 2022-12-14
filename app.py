bc687cc45cb1a63c428b251a41ea310d538db617
Anna Cai <anana10c@gmail.com>
2022-12-13 23:54:57 -0500

    dump

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import queue
-import numpy as np
+from collections import defaultdict
 import urllib
+
+import numpy as np
 import wikipedia
 import wikipedia2vec
 from wikipedia2vec import Wikipedia2Vec
 from wikimapper import WikiMapper
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+from flask_session import Session
+
+app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 MODEL_FILE = "D:/wikirec_storage/enwiki_20180420_100d.pkl"
 PAGERANK_FILE = "D:/wikirec_storage/2022-11-10.allwiki.links.rank"
@@ -33,11 +41,6 @@ def load_models():
 
 	print("mapper loaded!")
 
-	global like_feedback
-	like_feedback = set()
-	global dislike_feedback
-	dislike_feedback = set()
-
 
 def extract_title(link):
 	idx = link.rfind('/')
@@ -63,6 +66,7 @@ def compute_results(link, num=5, include_on_page=False):
 	except KeyError:
 		return []
 	root_id = mapper.title_to_id(root_title)
+	session["current_id"] = root_id
 
 	def compute_rank(title):
 		try:
@@ -75,13 +79,21 @@ def compute_results(link, num=5, include_on_page=False):
 		try:
 			wikidata_id = mapper.title_to_id(title)
 			page_score = np.log10(pagerank_dict[wikidata_id]) / 50
+
+			try:
+				feedback_key = tuple(sorted([root_id, wikidata_id]))
+				feedback_weight = 1.0 + 0.1 * session["feedback"][feedback_key]
+			except:
+				feedback_weight = 1.0
 		except KeyError:
 			# print(f"KeyError on page score for {title}")
+			wikidata_id = None
 			page_score = 0
+			feedback_weight = 1.0
 
-		return (content_score + page_score, title, content_score > page_score * 20)
+		return (feedback_weight * (content_score + page_score), title, 2 if feedback_weight > 1 else int(content_score > page_score * 20), wikidata_id)
 
-	checked = set(mapper.title_to_id(root_title))
+	checked = set(root_id)
 	on_page_results = queue.PriorityQueue(maxsize=20)
 	for link in root_page.links:
 		if mapper.title_to_id(link) not in checked:
@@ -95,7 +107,7 @@ def compute_results(link, num=5, include_on_page=False):
 	on_page_results = on_page_results.queue
 
 	results = queue.PriorityQueue(maxsize=num)
-	for _, root_link, _ in on_page_results:
+	for _, root_link, _, _ in on_page_results:
 		try:
 			secondary_page = wikipedia.page(root_link)
 		except:
@@ -116,13 +128,13 @@ def compute_results(link, num=5, include_on_page=False):
 		return list(reversed(sorted(results.queue)))[:min(num, len(results.queue))]
 
 
-def fetch_results(results):
+def fetch_display_results(results):
 	if results is None:
 		return None, "Sorry! There was an error in retrieving the page."
 	if len(results) == 0:
-		return None, "Sorry! We couldn't seem to find any results."
+		return None, "Sorry! We couldn't find any results."
 	data = []
-	for _, title, explanation in results:
+	for _, title, explanation, _ in results:
 		try:
 			page = wikipedia.page(title, auto_suggest=False)
 			summ = page.summary
@@ -131,9 +143,29 @@ def fetch_results(results):
 			url = page.url
 		except:
 			continue
-		explanation_str = "The content of this page is particularly relevant." if explanation else "This page has a relatively high importance."
+		if explanation == 2:
+			explanation_str = "This page was previously marked as a good recommendation through feedback."
+		elif explanation == 1:
+			explanation_str = "The content of this page is particularly relevant."
+		else:
+			assert explanation == 0
+			explanation_str = "This page has a relatively high importance."
 		data.append((pretty_title(title), url, summ, explanation_str))
 	return data, None
+
+
+def record_feedback(feedback_form):
+	if "current_id" not in session or session["current_id"] is None:
+		return "Sorry! Due to an error in retrieving the page ID, feedback could not be recorded."
+	
+	for i in range(len(session["results"])):
+		result_key = "result" + str(i)
+		if result_key in feedback_form:
+			wikidata_id = session["results"][i][3]
+			if wikidata_id is not None:
+				feedback_key = tuple(sorted([session["current_id"], wikidata_id]))
+				session["feedback"][feedback_key] += 1 if feedback_form[result_key] == "like" else -2
+	return "Feedback submitted!"
 
 
 # if __name__ == "__main__":
@@ -142,20 +174,41 @@ def fetch_results(results):
 # 	# link = "https://en.wikipedia.org/wiki/M%C4%81ori_language"
 # 	print(compute_results(link, 20, True))
 
-app = Flask(__name__)
-
-load_models()
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 	if request.method == "POST":
-		print(request.form)
-		link = request.form["wikilink"]
-		num = request.form["numResults"]
-		include_on_page = "checkPageLink" in request.form
-		results, message = fetch_results(compute_results(link, int(num), include_on_page))
-		# return f"POST successful with link {link}"
-		return render_template("index.html", results=results, message=message)
-	else:
-		return render_template("index.html", results=None, message=None)
+		if "search" in request.form:
+			link = request.form["wikilink"]
+			num = request.form["numResults"]
+			include_on_page = "checkPageLink" in request.form
+
+			session["link"] = link
+
+			results = compute_results(link, int(num), include_on_page)
+			session["results"] = results
+			display_results, message = fetch_display_results(results)
+			return render_template("index.html", results=display_results, message=message, link=link)
+		elif "feedback" in request.form:
+			message = record_feedback(request.form)
+			display_results, message2 = fetch_display_results(session["results"])
+			return render_template("index.html", results=display_results, message=message if message2 is None else message2, link=session["link"])
+		elif "clear" in request.form:
+			session["feedback"].clear()
+			message = "Feedback cleared!"
+			return render_template("index.html", results=None, message=message, link=None)
+		else:
+			raise NotImplementedError
+
+	return render_template("index.html", results=None, message=None, link=None)
+
+
+@app.before_first_request
+def setup():
+    load_models()
+    session["results"] = None
+    session["feedback"] = defaultdict(int)
+
+
+if __name__ == "__main__":
+	app.run()
